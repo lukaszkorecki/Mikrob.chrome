be799af6bd8d3f43d7bd9f9d0abed4a66c5825ea
Łukasz Korecki <lukasz@coffeesounds.com>
2010-12-25 18:25:22 +0000

    Simple http request class and blip api client class

--- js/blip.js
+++ js/blip.js
@@ -1,7 +1,28 @@
 var Blip = function(username, password) {
   this.username = username;
   this.password = password;
+  this.apiUrl = "http://api.blip.pl";
+  this.requestHeaders = {
+    'Accept' : 'application/json',
+    'X-Blip-Api' : '0.02',
+    'User-Agent' : 'Mikrob 0.2',
+    'X-Blip-Application' : 'Mikrob 0.2'
+  };
 };
 
 Blip.prototype.getDashboard = function(since){
+  var request = new Request(this.apiUrl, this.requestHeaders,{
+    username : this.username, password : this.password
+  });
+
+  request.get("/dashboard",{
+    onSuccess : function() {
+                  console.log("success");
+                  console.dir(arguments);
+                },
+    onFailure : function() {
+                  console.log("fail");
+                  console.dir(arguments);
+                }
+  });
 };
--- js/request.js
+++ js/request.js
@@ -4,12 +4,21 @@ var Request = function(url, requestHeaders, auth) {
   this.requestHeaders = requestHeaders || {};
 };
 
-Request.prototype.get = function(path, callbacks) {
+Request.prototype.objToQueryString = function(obj) {
+  var str = "";
+  for(field in obj) {
+    req.setRequestHeader(field, this.requestHeaders[field]);
+    str += encodeURIComponent(field) + "=" + encodeURIComponent(obj[field]);
+  }
+  return str;
+};
+
+Request.prototype.httpRequest = function(method, path, data, callbacks) {
   var req = new XMLHttpRequest();
 
-  req.open('get', path, true, this.auth.username, this.auth.password);
+  req.open(method, this.url+path, true, this.auth.username, this.auth.password);
 
-  for(header in this.requestHeaders) {
+  for(field in this.requestHeaders) {
     req.setRequestHeader(field, this.requestHeaders[field]);
   }
 
@@ -20,6 +29,7 @@ Request.prototype.get = function(path, callbacks) {
       switch(req.status) {
         case 200:
         case 201:
+        case 204:
           callbacks.onSuccess(req);
           break;
         default:
@@ -28,4 +38,16 @@ Request.prototype.get = function(path, callbacks) {
       }
     }
   };
+};
+Request.prototype.get = function(path, callbacks) {
+  this.httpRequest('get',path, false, callbacks);
+};
+Request.prototype.put = function(path, callbacks) {
+  this.httpRequest('post',path, false, callbacks);
+};
+Request.prototype['delete'] = function(path,callbacks) {
+  this.httpRequest('delete',path, false, callbacks);
+};
+Request.prototype.post = function(path,data, callbacks) {
+  this.httpRequest('post',path, data, callbacks);
 };
