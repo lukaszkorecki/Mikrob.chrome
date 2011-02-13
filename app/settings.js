0c3129839a4d5be7fdfcda492af79bdb0f86e9cd
Łukasz Korecki <lukasz@coffeesounds.com>
2011-02-13 21:49:50 +0000

    Settings panel now works as expected

--- app/settings.js
+++ app/settings.js
@@ -1,7 +1,9 @@
 // default settings if none are set
 var Settings = (function(){
+  // public
   var check = {};
 
+  // private
   function defaults() {
     var def = {
       refreshInterval : 1000,
@@ -14,41 +16,55 @@ var Settings = (function(){
     return def;
   }
 
+  // private
   function setFormData(data) {
-
     $('#preferences input').each(function(i,el){
-      $(el).attr('value', data[el.name]);
+      // this can be changed to a regex later on
+      // so that diff types are accepted
+      if(el.type == 'hidden') {
+        $(el).attr('value', data[el.name]);
+      }
     });
+    var selector = data.notificationsEnabled ? 0 : 1;
+    $('#preferences input[type=radio]').dom[selector].checked = true;
   }
 
-  function getFormData(data) {
+  // private
+  function getFormData() {
+    var data = {};
     $('#preferences input').each(function(i,el){
-      data[el.name] = JSON.parse(el.val());
+      if(el.type == 'hidden') {
+        data[el.name] = JSON.parse(el.value);
+      } else if(el.type == 'radio'){
+        // assume that if it's not enabled, then it's disabled
+        var enabled = !! $('#preferences input[type=radio]').dom[0].checked;
+        data[el.name] = enabled;
+      }
     });
 
     return data;
   }
 
-  // @public
+  // public
   function load() {
     if(!! localStorage.mikrob_preferences) {
       this.check = JSON.parse(localStorage.mikrob_preferences);
     } else {
       this.check = defaults();
       localStorage.mikrob_preferences = JSON.stringify(this.check);
-      setFormData(this.check);
-
     }
+    setFormData(this.check);
   }
 
+  // public
   function save() {
-    this.check = getFormData({});
+    this.check = getFormData();
     localStorage.mikrob_preferences = JSON.stringify(this.check);
   }
 
   return {
     check : check,
-    load : load,
-    save : save
+          load : load,
+          save : save
   };
 })();
