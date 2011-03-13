e4ed49657a306602d209ed094e50af3a189a98ab
Łukasz Korecki <lukasz@coffeesounds.com>
2011-03-13 15:06:22 +0000

    First attempt: works as expected, needs checking file POST and checking why POST in general is ok, but uses onFailure callback

---
+++ blip_oauth_example_conf.js
@@ -0,0 +1,12 @@
+var BlipOAuthData = {
+  key : 'TROLOLLOLOLO',
+  secret: 'HAI GUIZ',
+  url : {
+    root : 'http://blip.pl/oauth',
+    request_token : '/request_token',
+    access_token : '/access_token',
+    authorize : '/authorize'
+
+  }
+};
+
