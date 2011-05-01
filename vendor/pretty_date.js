d54a975c853c1d7418e615ad21958b8e9be0b716
Łukasz Korecki <lukasz@coffeesounds.com>
2011-05-01 22:49:53 +0100

    pretty date kinda translated, doesnt take timezones into account

--- vendor/pretty_date.js
+++ vendor/pretty_date.js
@@ -1,28 +1,29 @@
 function PrettyDate(date_str){
   var time_formats = [
-    [60, 'just now', 1], // 60
-    [120, '1 minute ago', '1 minute from now'], // 60*2
-    [3600, 'minutes', 60], // 60*60, 60
-    [7200, '1 hour ago', '1 hour from now'], // 60*60*2
-    [86400, 'hours', 3600], // 60*60*24, 60*60
-    [172800, 'yesterday', 'tomorrow'], // 60*60*24*2
-    [604800, 'days', 86400], // 60*60*24*7, 60*60*24
-    [1209600, 'last week', 'next week'], // 60*60*24*7*4*2
-    [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
-    [4838400, 'last month', 'next month'], // 60*60*24*7*4*2
-    [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+    [60, '', 1], // 60
+    [120, 'minutę temu', 'za minutę'], // 60*2
+    [3600, 'minut', 60], // 60*60, 60
+    [7200, 'godzinę', 'za godzinę'], // 60*60*2
+    [86400, 'godzin', 3600], // 60*60*24, 60*60
+    [172800, 'wczoraj', 'jutro'], // 60*60*24*2
+    [604800, 'dni', 86400], // 60*60*24*7, 60*60*24
+    [1209600, 'w zeszłym tygodniu', 'w przyszłym tygodniu'], // 60*60*24*7*4*2
+    [2419200, 'tygodni', 604800], // 60*60*24*7*4, 60*60*24*7
+    [4838400, 'w zeszłym miesiącu', 'w następnym miesiącu'], // 60*60*24*7*4*2
+    [29030400, 'miesięcy', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+    // hm....
     [58060800, 'last year', 'next year'], // 60*60*24*7*4*12*2
     [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
     [5806080000, 'last century', 'next century'], // 60*60*24*7*4*12*100*2
     [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
   ];
-  var time = ('' + date_str).replace(/-/g,"/").replace(/[TZ]/g," ").replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+  var time = date_str; //('' + date_str).replace(/-/g,"/").replace(/[TZ]/g," ").replace(/^\s\s*/, '').replace(/\s\s*$/, '');
   if(time.substr(time.length-4,1)==".") time =time.substr(0,time.length-4);
   var seconds = (new Date - new Date(time)) / 1000;
-  var token = 'ago', list_choice = 1;
+  var token = 'temu', list_choice = 1;
   if (seconds < 0) {
     seconds = Math.abs(seconds);
-    token = 'from now';
+    token = '';
     list_choice = 2;
   }
   var i = 0, format;
@@ -34,4 +35,4 @@ function PrettyDate(date_str){
         return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
     }
   return time;
-};
+}
