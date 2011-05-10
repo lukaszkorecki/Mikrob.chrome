079e9dd56705ce462c16f9e06d86e61f04648f01
Łukasz Korecki <lukasz@coffeesounds.com>
2011-05-10 16:46:22 +0100

    remove obsolete func

--- app/service.js
+++ app/service.js
@@ -204,25 +204,6 @@ Mikrob.Service = (function(){
     });
   }
 
-  function normalizePolishChars(string) {
-    var charMap = {
-      'ą' : 'a',
-      'ż' : 'z',
-      'ź' : 'z',
-      'ń' : 'n',
-      'ó' : 'o',
-      'ę' : 'e',
-      'ć' : 'c',
-      'ł' : 'l'
-    };
-
-    var normalized_string = string;
-
-    for(var letter in charMap) {
-      normalized_string = normalized_string.replace(letter, charMap[letter]);
-    }
-    return normalized_string;
-  }
 
   function getTag(tag) {
 
